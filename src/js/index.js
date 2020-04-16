63f0db61c22441755054a835dae7b43efbf25e0d
digtiar23 <artemdigtiar@gmail.com>
2020-04-15 22:20:08 -0700

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+import obj from './config/config'
+import '../css/style.css'
+import json from '../assets/json'
+
+console.log("Hello world!")
+
+console.log(obj);
+
+console.log(json);
+console.log('12312312312321');
+
+class Class {
+
+
+    getC() {
+        console.log(1)
+    }
+}
